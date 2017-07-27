1ab09b75f1e032610ac128e0c5f259f78ee33655
fa <komidin10@gmail.com>
2017-07-27 17:03:21 +0300

    C#'ta Encapsulation ve Property ornekleri

---
+++ Encapsulation/Encapsulation/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encapsulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Ogrenci o = new Ogrenci();
+
+            try
+            {
+                o.Adi = txtAdi.Text;
+                o.DogumTarihi = dateTimePicker1.Value;
+                o.IseGiris = dateTimePicker2.Value;
+                o.PrimFiyat = Convert.ToDouble(txtPrim.Text);
+                MessageBox.Show("Sayin " + o.Adi + "\n\nYas: " + o.Yas.ToString() +
+                    "\nCalistigi Gun Sayisi: " + o.calistigiGun.ToString() +
+                    "\nTazminati: " + o.PrimFiyat.ToString() + " TL");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtAdi.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            Ogrenci o = new Ogrenci();
+            try
+            {
+                o.Adi = txtAdi.Text;
+                o.Vize = Convert.ToDouble(txtVize.Text);
+                o.Final = Convert.ToDouble(txtFinal.Text);
+                MessageBox.Show("Sayin " + o.Adi + "\n\nNot Ortalamasi: " + o.Ortalama);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Ogrenci.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Ogrenci
+    {
+        private string adi;
+
+        public string Adi
+        {
+            get { return adi; }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new Exception("Lutfen Isminizi Giriniz !");
+                else
+                    adi = value;
+            }
+        }
+
+        private DateTime dogumTarihi;
+        public DateTime DogumTarihi
+        {
+            get { return this.dogumTarihi; }
+            set
+            {
+                if (value > DateTime.Now)
+                    throw new Exception("Bugunun tarihinde buyuk atanamaz...!");
+                else
+                    this.dogumTarihi = value;
+
+            }
+        }
+        public int Yas
+        {
+            get
+            {
+                return (int)((DateTime.Now - this.DogumTarihi).Days / 365);
+            }
+        }
+
+        private DateTime iseGiris;
+        public DateTime IseGiris
+        {
+            get { return this.iseGiris; }
+            set
+            {
+                if (value > DateTime.Now)
+                    throw new Exception("Bugunun tarihinden buyuk deger atayamazsiniz...!");
+                else
+                    this.iseGiris = value;
+
+            }
+        }
+
+        public int calistigiGun
+        {
+            get
+            {
+                return (int)((DateTime.Now - this.IseGiris).TotalDays);
+            }
+        }
+
+        private double prim;
+        public double PrimFiyat
+        {
+            get
+            {
+                return this.prim * this.calistigiGun;
+            }
+            set
+            {
+                if (value < 10)
+                {
+                    throw new Exception("Prim 10 TL'den kucuk olamaz\n10 TL minimum olarak ayarlandi !");
+                    value = 10;
+
+                }
+                else if (value > 50)
+                {
+                    throw new Exception("Tazminat 50 TL'den buyuk olamaz\n50 TL default ayarlandi !");
+                    value = 50;
+
+                }
+                this.prim = value;
+            }
+
+        }
+
+
+
+        double vize;
+        double final;
+        public double Vize
+        {
+            get
+            {
+                return this.vize;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                    this.vize = value;
+                else
+                    throw new Exception("Yanlis Vize araligi girdiniz !!!");
+            }
+        }
+        public double Final
+        {
+            get
+            {
+                return this.final;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                    this.final = value;
+                else
+                    throw new Exception("Yanlis Final araligi girdiniz !!!");
+            }
+        }
+        public double Ortalama
+        {
+            get
+            {
+                return (Vize * 0.4) + (Final * 0.6);
+            }
+        }
+
+    }
+}
